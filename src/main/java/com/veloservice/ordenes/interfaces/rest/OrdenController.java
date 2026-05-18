7b0a60ebecbdfe2fdbd6af475b10409517a5872c
Oscar Munoz <osca.munozs@duocuc.cl>
2026-05-18 09:16:41 -0400

    feat: add PATCH /ordenes/{id}/estado alias alongside existing PUT

--- src/main/java/com/veloservice/ordenes/interfaces/rest/OrdenController.java
+++ src/main/java/com/veloservice/ordenes/interfaces/rest/OrdenController.java
@@ -12,6 +12,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -137,6 +138,7 @@ public class OrdenController {
      * Changes the state of a work order.
      */
     @PutMapping("/{id}/estado")
+    @PatchMapping("/{id}/estado")
     public ResponseEntity<OrdenResponse> cambiarEstado(
             @PathVariable UUID id,
             @Valid @RequestBody EstadoChangeRequest request) {
